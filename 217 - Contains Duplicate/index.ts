52acc77c7df8b35bfa6e95d2ac14fc99ab640b44
WildBabaloo <andphiltre@gmail.com>
2024-04-20 22:41:32 -0400

    217 - Contains Duplicate Complete

---
+++ 217 - Contains Duplicate/index.ts
@@ -0,0 +1,18 @@
+function containsDuplicate(nums: number[]): boolean {
+    const sortedArray = nums.sort();
+    for (let i = 0; i < sortedArray.length; i++) {
+        if (sortedArray[i] === sortedArray[i + 1]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+console.log(containsDuplicate([1,2,3,1]));
+console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+
+
+
