392c4551f86f7834873d97b810672ad9456fd8f9
WildBabaloo <andphiltre@gmail.com>
2024-04-20 22:42:13 -0400

    Removed a space

--- 217 - Contains Duplicate/index.ts
+++ 217 - Contains Duplicate/index.ts
@@ -9,7 +9,6 @@ function containsDuplicate(nums: number[]): boolean {
     return false;
 }
 
-
 console.log(containsDuplicate([1,2,3,1]));
 console.log(containsDuplicate([1,2,3,4]));
 console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
