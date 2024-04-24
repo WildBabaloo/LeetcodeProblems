0bf367eb353ddb606df93e5c00c131c3101deec5
WildBabaloo <andphiltre@gmail.com>
2024-04-24 03:26:13 -0400

    347. Top K Frequent Elements complete!

---
+++ 347 - Top K Frequent Elements/index.ts
@@ -0,0 +1,12 @@
+function topKFrequent(nums: number[], k: number): number[] {
+    const hashmap = new Map<number, number>();
+    for (const num of nums) {
+        hashmap.set(num, (hashmap.get(num) ?? 0) + 1)
+    }
+
+    const hashmapSort:Map<number, number> = new Map([...hashmap.entries()].sort((a, b) => b[1] - a[1]));
+    return [...hashmapSort.keys()].slice(0, k);
+}
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+console.log(topKFrequent([1], 1));
